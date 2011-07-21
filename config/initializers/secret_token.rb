0a8cf71965d7d27c35960fac3a04da7fb5fb64b0
Jim V <vlandham@gmail.com>
2011-07-20 22:55:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salonpals::Application.config.secret_token = '5a3ad3dc3589971cea5fcd7534f54885b13dcd354788c24840ac2d505be34835db071c51f7893f03a9f1805425f8187b4a2c1b1b8b482bb5f04671a3c682bdfa'
