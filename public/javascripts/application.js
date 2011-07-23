946f5a8116460b92021f250c83c10d45631bef15
Jim V <vlandham@gmail.com>
2011-07-22 23:04:33 -0500

    parameters on what controls to use for formatted text

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,5 +7,14 @@ jQuery(document).ready(function($) {
     $(".notice").delay("2000").slideUp("slow")
   }
   
-   $(".text").cleditor();
+   $(".text").cleditor({
+     width:        500,
+     height:       250,
+     controls: 
+      "bold italic underline | font size " +
+      "style | color highlight removeformat | bullets numbering | outdent " +
+      "indent | alignleft center alignright justify | undo redo | " +
+      "rule | cut copy paste pastetext"
+     
+   });
 });
