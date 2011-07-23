d95bdd0ef81dec6f5094e5817f00fe9220ed4cb8
Jim V <vlandham@gmail.com>
2011-07-22 22:27:17 -0500

    some jquery fun

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function($) {
+  $("#language-button").click(function() {
+    $("#language-selection").toggle();
+  });
+  
+  if ($(".notice").length) {
+    $(".notice").delay("2000").slideUp("slow")
+  }
+});
