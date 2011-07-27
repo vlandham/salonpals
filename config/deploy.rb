e59108e0b13cb2902dd4a6e9814d76baa173559f
Jim V <vlandham@gmail.com>
2011-07-26 21:23:47 -0500

    fixes for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,16 @@
-set :application, "salonpals"
-set :repository,  "git@repo:salonpals"
+require "bundler/capistrano"
 
-set :deploy_via, :remote_cache
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2@rails3'        # Or whatever env you want it to run in.
+set :rvm_bin_path, "/usr/local/rvm/bin"
+
+set :application, "salonpals"
+set :repository,  "git@10.0.1.5:salonpals"
+
+set :deploy_via, :copy
 set :scm, :git
+set :scm_username, "vlandham"
 
 
 set :user, 'ubuntu'
@@ -17,17 +25,17 @@ role :db,  "ec2-107-20-17-143.compute-1.amazonaws.com", :primary => true # This 
 
 set :deploy_to, "/var/rails/#{application}"
 
-before "deploy", "deploy:bundle_gems"
-before "deploy:restart", "deploy:bundle_gems"
+#before "deploy", "deploy:bundle_gems"
+#before "deploy:restart", "deploy:bundle_gems"
 
 set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
-set :bundle, "bundle"
+#set :bundle, "bundle"
 set :unicorn_rails, "unicorn_rails"
 
 namespace :deploy do
-  task :bundle_gems do
-    run "cd #{deploy_to}/current && #{bundle} install --path vendor/gems" 
-  end
+#  task :bundle_gems do
+#    run "cd #{deploy_to}/current && #{bundle} install --path vendor/gems" 
+#  end
 
   task :start, :roles => :app, :except => { :no_release => true} do
     run "#{unicorn_rails} -c #{deploy_to}/current/config/unicorn.rb -D -E production"
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -32,7 +32,7 @@ listen 8080, :tcp_nopush => true
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/var/rails/salonpals/shared/tmp/pids/unicorn.pid"
+pid "/var/rails/salonpals/shared/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
