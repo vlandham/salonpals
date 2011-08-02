298949aa9873019db16a2cff964c6372c268b3ec
Jim V <vlandham@gmail.com>
2011-08-01 22:38:39 -0500

    cleanup

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,14 +1,13 @@
 class LocalesController < ApplicationController
   def show
-    new_locale = valid_locale?(params[:id]) ? params[:id] : LANGUAGES.first.first
+    new_locale = valid_locale?(params[:id]) ? params[:id] : default_locale
     if user_signed_in?
       @user = current_user
       @user.language = new_locale
-      @user.save!
+      @user.save
     end
     
     redirect_to posts_path(params[:locale] = new_locale)
-      
   end
   
   private
@@ -17,5 +16,7 @@ class LocalesController < ApplicationController
     LANGUAGES.keys.include? locale
   end
   
-  
+  def default_locale
+    LANGUAGES.first.first
+  end  
 end
