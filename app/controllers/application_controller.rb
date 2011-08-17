f5ed9d597f2569b0f1d46988d3f07fa95bd4cff5
Jim V <vlandham@gmail.com>
2011-08-16 22:08:37 -0500

    fix mobile switching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   private
   
   def mobile_device?
-    if session[:mobile_device]
+    if session[:mobile_param]
       session[:mobile_param] == "1"
     else
       request.user_agent =~ /Mobile|webOS/
